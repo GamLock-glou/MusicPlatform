cce491916c0f7a538dca5955f3ca1bcad5328e89
GamLock-glou <klimenkov.z@mail.ru>
2023-07-17 22:28:50 +0300

    Deploy back

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,10 @@ const start = async () => {
     const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-      origin: ['https://music-platform-ten.vercel.app'],
+      origin: [
+        'http://localhost:3000',
+        'https://music-platform-ten.vercel.app',
+      ],
       methods: ['POST', 'PUT', 'DELETE', 'GET'],
     });
     await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
