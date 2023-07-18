777888a5b3a2f19e00550ec8751c724a1bfb5b6b
GamLock-glou <klimenkov.z@mail.ru>
2023-07-18 18:14:19 +0300

    Add logic from api

---
+++ server/src/album/album.mobule.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileService } from 'src/file/file.service';
+import { AlbumController } from './album.controller';
+import { AlbumService } from './album.service';
+import { Album, AlbumSchema } from './schemas/album.schema';
+
+@Module({
+  imports: [
+    // MongooseModule.forFeature([{ name: Track.name, schema: TrackSchema }]),
+    MongooseModule.forFeature([{ name: Album.name, schema: AlbumSchema }]),
+  ],
+  controllers: [AlbumController],
+  providers: [AlbumService, FileService],
+})
+export class AlbumModule {}
